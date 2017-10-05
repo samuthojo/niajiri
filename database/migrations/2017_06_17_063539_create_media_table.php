3ddf3ab53f57b48b0dac39103f3ce821ba2bbb44
lykmapipo <lallyelias87@gmail.com>
2017-10-05 11:50:56 +0300

    add dependencies migrations

---
+++ database/migrations/2017_06_17_063539_create_media_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMediaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('medias', function (Blueprint $table) {
+            $table->uuid('id');
+
+            // $table->morphs('model');
+            $table->uuid('model_id');
+            $table->string('model_type');
+
+            $table->string('collection_name');
+            $table->string('name');
+            $table->string('file_name');
+            $table->string('mime_type')->nullable();
+            $table->string('disk');
+            $table->unsignedInteger('size');
+            $table->text('manipulations');
+            $table->text('custom_properties');
+            $table->unsignedInteger('order_column')->nullable();
+            $table->nullableTimestamps();
+
+            $table->primary('id');
+
+        });
+    }
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        Schema::dropIfExists('medias');
+    }
+}
