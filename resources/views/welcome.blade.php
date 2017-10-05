c4e4e124470674a629ce57e7de6db755200f698f
lykmapipo <lallyelias87@gmail.com>
2017-10-05 08:22:36 +0300

    update dependencies

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,17 +5,13 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <title>niajiri</title>
 
         <!-- Styles -->
         <style>
             html, body {
                 background-color: #fff;
                 color: #636b6f;
-                font-family: 'Raleway', sans-serif;
                 font-weight: 100;
                 height: 100vh;
                 margin: 0;
@@ -79,7 +75,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    niajiri
                 </div>
 
                 <div class="links">
