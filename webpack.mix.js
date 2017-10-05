9b839a912092f0d9a2f2d2c1d220d636e4e10403
lykmapipo <lallyelias87@gmail.com>
2017-10-05 08:18:41 +0300

    fix mix pipeline for font copying

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,9 +12,5 @@ let mix = require('laravel-mix');
  */
 
 mix
-    .copy('node_modules/simple-line-icons/fonts', 'public/fonts')
-    .copy('node_modules/font-awesome/fonts', 'public/fonts')
-    .copy('node_modules/lato/fonts', 'public/fonts')
-    .copy('node_modules/bootstrap-colorpicker/dist/img', 'public/img')
     .js('resources/assets/js/app.js', 'public/js')
     .sass('resources/assets/sass/app.scss', 'public/css');
