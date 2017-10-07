a2d8326cfeb90f406974a0eecd5588d6e36e1b2f
lykmapipo <lallyelias87@gmail.com>
2017-10-07 11:03:25 +0300

    initial social service configuration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\AuditDrivers\Niajiri;
 use Illuminate\Support\ServiceProvider;
 use OwenIt\Auditing\Facades\Auditor;
+use Illuminate\Support\Facades\Config;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -30,5 +31,14 @@ class AppServiceProvider extends ServiceProvider
     {
         //register helpers
         require_once __DIR__ . '/../Helpers/strings.php';
+
+        //register oauth callback uri & urls
+        foreach (Config::get('services') as $key => $config) {
+
+            if (!isset($config['redirect'])) {
+                continue;
+            }
+            Config::set("services.$key.redirect", url($config['redirect']));
+        }
     }
 }
