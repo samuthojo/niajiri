2301c3ab5fa4395f069865fba948f999c140466a
MAKOSCAFEE <barnabasmakonda@gmail.com>
2017-10-07 13:40:30 +0300

    update organization projects

--- resources/views/pages/organizations/projects.blade.php
+++ resources/views/pages/organizations/projects.blade.php
@@ -2,74 +2,35 @@
   <table class="table table-striped">
       <thead>
       <tr>
-          <th>Status</th>
-          <th>Title</th>
-          <th>Start Time</th>
-          <th>End Time</th>
-          <th>Comments</th>
+          <th class="text-center">Status</th>
+          <th class="text-center">Title</th>
+          <th class="text-center">Start Time</th>
+          <th class="text-center">End Time</th>
+          <th class="text-center">Actions</th>
       </tr>
       </thead>
       <tbody>
-      <tr>
-          <td>
-              <span class="label label-primary">Active</span>
-          </td>
-          <td>
-             Create project in webapp
-          </td>
-          <td>
-             12.07.2014 10:10:1
-          </td>
-          <td>
-              14.07.2014 10:16:36
-          </td>
-          <td>
-          <p class="small">
-              Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable.
-          </p>
-          </td>
+        @foreach($organization->projects as $item)
+          <tr>
+              <td class="text-center">
+                  <span class="label label-primary">Active</span>
+              </td>
+              <td class="text-center">
+                 {{$item->name}}
+              </td>
+              <td class="text-center">
+                 {{$item->startedAt}}
+              </td>
+              <td class="text-center">
+                  {{$item->endedAt}}
+              </td>
+              <td class="project-actions text-center">
+                  <a href="#" class="btn btn-white btn-sm"><i class="fa fa-folder"></i> View </a>
+                  <a href="#" class="btn btn-white btn-sm"><i class="fa fa-pencil"></i> Edit </a>
+              </td>
 
-      </tr>
-      <tr>
-          <td>
-              <span class="label label-default">Inactive</span>
-          </td>
-          <td>
-              Various versions
-          </td>
-          <td>
-              12.07.2014 10:10:1
-          </td>
-          <td>
-              14.07.2014 10:16:36
-          </td>
-          <td>
-              <p class="small">
-                  Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
-              </p>
-          </td>
-
-      </tr>
-      <tr>
-          <td>
-              <span class="label label-primary">Active</span>
-          </td>
-          <td>
-              There are many variations
-          </td>
-          <td>
-              12.07.2014 10:10:1
-          </td>
-          <td>
-              14.07.2014 10:16:36
-          </td>
-          <td>
-              <p class="small">
-                  There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which
-              </p>
-          </td>
-
-      </tr>
+          </tr>
+          @endforeach
 
       </tbody>
   </table>
