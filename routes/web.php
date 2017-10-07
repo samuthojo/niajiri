3f7d2e6ccd876c2cc512c54c068c1c26dc64a1f5
lykmapipo <lallyelias87@gmail.com>
2017-10-07 12:16:06 +0300

    update mail server configs

--- routes/web.php
+++ routes/web.php
@@ -36,11 +36,10 @@ Route::resource('positions', 'PositionController');
 
 
 //social auth routes
-Route::group(['middleware' => ['web', 'guest']], function () {
 
-    Route::get('/auth/social/provider/{name}', 'Auth\SocialAuthController@signIn')->name('auth.social.provider');
-    Route::get('/auth/social/callback/{name}', 'Auth\SocialAuthController@signInCallback')->name('auth.social.callback');
-    Route::get('/auth/social/existing-user', 'Auth\SocialAuthController@getCurrentUserSignIn')->name('auth.social.existing.user');
-    Route::post('/auth/social/existing-user', 'Auth\SocialAuthController@postCurrentUserSignin');
-
-});
+Route::get('/auth/social/provider/{name}', 'Auth\SocialAuthController@signIn')
+	->name('auth.social.provider');
+Route::get('/auth/social/callback/{name}', 'Auth\SocialAuthController@signInCallback')
+	->name('auth.social.callback');
+Route::get('/auth/social/existing-user', 'Auth\SocialAuthController@getCurrentUserSignIn')->name('auth.social.existing.user');
+Route::post('/auth/social/existing-user', 'Auth\SocialAuthController@postCurrentUserSignin');
