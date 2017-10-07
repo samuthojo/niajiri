0488d3a3b8337018e3b97819edfe153a9ad3aa60
lykmapipo <lallyelias87@gmail.com>
2017-10-07 11:23:59 +0300

    add social login buttons

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -24,9 +24,41 @@
 				@endif
 			</div>
 
-			<button type="submit" class="btn btn-primary block full-width m-b">Log in</button>
+			<button type="submit" class="btn btn-primary block full-width m-b">Signin</button>
 
-			{{-- <a href="{{ route('password.request') }}"><small>Forgot password?</small></a> --}}
+			<p class="text-muted text-center"><small>OR</small></p>
+
+			{{-- start social signin --}}
+
+            {{-- start google plus auth --}}
+			<a class="btn btn-danger btn-google block full-width m-b"
+				href="{{url(config('services.google.url'))}}">
+			   <span class="fa fa-google"></span> Signin with Google
+			</a>
+			{{-- end google plus auth --}}
+
+			{{-- start facebook auth --}}
+			<a class="btn btn-success btn-facebook block full-width m-b"
+				href="{{url(config('services.facebook.url'))}}">
+		       <span class="fa fa-facebook"></span> Signin with Facebook
+		    </a>
+		    {{-- end facebook auth --}}
+
+		    {{-- start twitter auth --}}
+			<a class="btn btn-info btn-twitter block full-width m-b"
+				href="{{url(config('services.twitter.url'))}}">
+		       <span class="fa fa-twitter"></span> Signin with Twitter
+		    </a>
+		    {{-- end twitter auth --}}
+
+		    {{-- start linkedin auth --}}
+			<a class="btn btn-success btn-linkedin block full-width m-b"
+				href="{{url(config('services.linkedin.url'))}}">
+		       <span class="fa fa-linkedin"></span> Signin with LinkedIn
+		    </a>
+		    {{-- end linkedin auth --}}
+
+            {{-- end social signin --}}
 		</form>
-	</diV>
+	</div>
 @endsection
