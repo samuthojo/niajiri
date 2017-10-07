4cb67a6e4e81696978e477cfd19d871f7dc18bd6
lykmapipo <lallyelias87@gmail.com>
2017-10-07 09:21:58 +0300

    refactor redirect to config

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo;
 
     /**
      * Create a new controller instance.
@@ -34,6 +34,8 @@ class LoginController extends Controller
      */
     public function __construct()
     {
+        parent::__construct();
+        $this->redirectTo = config('auth.defaults.redirect');
         $this->middleware('guest')->except('logout');
     }
 }
