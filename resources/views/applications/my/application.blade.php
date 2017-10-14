1b3812cee681feb30d3cd9c8907c758a5e03f52c
lykmapipo <lallyelias87@gmail.com>
2017-10-14 08:57:35 +0300

    clear unused frontend dependencies

--- resources/views/applications/my/application.blade.php
+++ resources/views/applications/my/application.blade.php
@@ -81,7 +81,7 @@
                                     <a href="#">
                                         <span class="corner"></span>
 
-                                        <div class="icon">
+                                        <div class="icon photoUpload">
                                             <i class="fa fa-file"></i>
                                         </div>
                                         <div class="file-name">
@@ -90,7 +90,16 @@
                                             <small>Added: Jan 11, 2014</small>
                                         </div>
                                     </a>
+                                    <div class="change-photo-btn change-photo-btn-cover-letter">
+                                    <div class="photoUpload">
+                                        <span title="{{trans('educations.inputs.attachment.placeholder')}}">
+                                            <i class="fa fa-upload"></i> {{trans('educations.inputs.attachment.change')}}
+                                        </span>
+                                        <input id="attachment" name="attachment" type="file" class="upload" />
+                                    </div>
                                 </div>
+                                </div>
+                            </div>
                             </div>
                         </div>
                     </div>
