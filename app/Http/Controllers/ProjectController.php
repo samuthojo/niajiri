6989301f114b0d7e6ed1f65bcc6633032f7e1731
Barnabas Makonda <barnabasmakonda@gmail.com>
2017-12-14 10:37:52 +0300

    fix link for applying position

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use App\Models\Position;
-use App\Models\Project;
 use App\Http\Requests\CreateProjectRequest;
 use App\Http\Requests\UpdateProjectRequest;
 use App\Repositories\ProjectRepository;
