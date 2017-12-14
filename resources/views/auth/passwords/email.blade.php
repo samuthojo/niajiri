f193fe103423edf1a03fd94c53cc66366dd44a15
lykmapipo <lallyelias87@gmail.com>
2017-12-14 12:44:44 +0300

    ensure valid email templates

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,46 +1,54 @@
 @extends('layouts.auth')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Reset Password</div>
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form class="form-horizontal" role="form" method="POST" action="{{ route('password.email') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Send Password Reset Link
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+   {{-- @if (session('status'))
+        <div class="alert alert-success">
+            {{ session('status') }}
         </div>
+    @endif
+    --}}
+    <div class="middle-box text-center loginscreen animated fadeInDown">
+        <img src="{{ asset('images/logo.png') }}" width="160" height="120">
+           {!!
+                Form::open([
+                    'route' => 'password.email',
+                    'files' => true
+                ])
+            !!}
+
+             <div class="ibox-content">
+
+                    <h2 class="font-bold">Forgot password</h2>
+
+                    <p>
+                        Enter your email address and your password will be reset and emailed to you.
+                    </p>
+
+                    <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                        {!! Form::email('email', null, [
+                            'id' => 'email',
+                            'class' => 'form-control',
+                            'required' => 'required',
+                            'placeholder' => 'Email'
+                        ]) !!}
+                        @if ($errors->has('email'))
+                            <span class="help-block">
+                                <strong>{{ $errors->first('email') }}</strong>
+                            </span>
+                        @endif
+                    </div>
+
+                    {!!
+                        Form::button(
+                            'Send Password Reset Link',
+                            [
+                            'type' => 'submit',
+                            'class' => 'btn btn-primary block full-width m-b',
+                            'title' => 'Click to Send Password Reset Link',
+                        ])
+                    !!}
+
+                {!! Form::close() !!}
+            </div>
     </div>
-</div>
 @endsection
