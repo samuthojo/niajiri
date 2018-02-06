e4811189069270e6c2e951b8eb576bdec18a0602
lykmapipo <lallyelias87@gmail.com>
2018-02-06 15:14:02 +0300

    commit all local changes

--- config/queue.php
+++ config/queue.php
@@ -15,7 +15,7 @@ return [
     |
 	*/
 
-	'default' => env('QUEUE_DRIVER', 'sync'),
+	'default' => $isDev ? 'sync' : env('QUEUE_DRIVER', 'sync'),
 
 	/*
     |--------------------------------------------------------------------------
