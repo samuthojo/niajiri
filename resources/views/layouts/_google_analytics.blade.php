bbf641a60cce0026cd216f0e60df58c62bbefeac
Nelson Malekela <nellyocs90@gmail.com>
2018-03-14 11:18:30 +0300

    Change google analytics quotations

--- resources/views/layouts/_google_analytics.blade.php
+++ resources/views/layouts/_google_analytics.blade.php
@@ -1,8 +1,8 @@
-<script async defer src=“https://www.googletagmanager.com/gtag/js?id=UA-70765388-9“></script>
+<script async defer src="https://www.googletagmanager.com/gtag/js?id=UA-70765388-9"></script>
 <script>
- window.dataLayer = window.dataLayer || [];
- function gtag(){dataLayer.push(arguments);}
- gtag('js', new Date());
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
- gtag('config', 'UA-70765388-9');
+  gtag('config', 'UA-70765388-9');
 </script>
